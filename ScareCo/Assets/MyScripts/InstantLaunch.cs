86486eca008932e2925def3fd77b6bf05615a753
jsoliv2 <57681366+jsoliv2@users.noreply.github.com>
2021-11-20 18:08:11 -0600

    Added scenes with modifications

---
+++ ScareCo/Assets/MyScripts/InstantLaunch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantLaunch : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    void Start()
+    {
+        if (rb != null)
+        {
+            rb.AddForce(new Vector3(0f, 0f, -25f), ForceMode.Impulse);
+        }
+    }
+}
---
+++ ScareCo/Assets/MyScripts/PlaneDrop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDrop : MonoBehaviour
+{
+    [SerializeField] private GameObject paratrooper;
+    [SerializeField] private Transform spawnLocation;
+
+    public void DropParatrooper()
+    {
+        Instantiate(paratrooper, spawnLocation.position, Quaternion.identity);
+    }
+}
