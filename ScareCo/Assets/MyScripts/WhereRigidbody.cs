6c7ac3b6064a0b929f854882d8504fe17da4c1ea
jsoliv2 <57681366+jsoliv2@users.noreply.github.com>
2021-11-23 13:40:09 -0600

    Added figures and lamp to Mego scene.

---
+++ ScareCo/Assets/MyScripts/WhereRigidbody.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhereRigidbody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody[] rbs = GetComponentsInChildren<Rigidbody>();
+        Debug.Log(rbs.Length);
+        foreach (Rigidbody rb in rbs)
+        {
+            Destroy(rb);
+        }
+    }
+}
